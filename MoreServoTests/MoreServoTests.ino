71ac6f0bd8cc166d7c7424ae4b44b49884150296
MatthewBennington <Matt@Matthews-MacBook-Pro.local>
2016-04-06 00:47:43 -0400

    Work done Night before 4/5 class

--- MoreServoTests/MoreServoTests.ino
+++ MoreServoTests/MoreServoTests.ino
@@ -5,23 +5,24 @@
  modified 8 Nov 2013
  by Scott Fitzgerald
  http://www.arduino.cc/en/Tutorial/Sweep
-
- Modified by Matt Jones March 2016
 */
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo servo1;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
+int pos = 0;    // variable to store the servo position
+
 void setup() {
-  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  servo1.attach(3);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  myservo.write(180);
-  delay(random(5000, 10000));
-  myservo.write(170); 
-  delay(500);//(random(5000, 10000));
+
+  pos += 1;
+
+  servo1.write(pos);
+  delay(15);
   
 }
---
+++ SerialTests/SerialTests.ino
@@ -0,0 +1,13 @@
+//#include <SoftwareSerial.h>
+
+import processing.serial.*;
+
+void setup() {
+ Serial.begin(9600);
+}
+ 
+void loop() {
+ int analogValue = analogRead(A0)/4; // read the sensor value
+ Serial.write(analogValue);          // send the value serially
+                                     // as a binary value
+}
